a7eb680ed7263ae6fbc27b42ef477bc3931aba52
Nataly Makarova <natmaklis@gmail.com>
2020-05-18 16:19:44 +0300

    add conect(redux) in to  basketItem  component

--- src/components/card__basketDropdown/CardBasketDropdown.jsx
+++ src/components/card__basketDropdown/CardBasketDropdown.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import CustomButton from "../custom__button/CustomButton.jsx";
 import "./card__basketDropdown.scss";
+import BasketItem from "../basket__item/BasketItem.jsx";
+import { connect } from "react-redux";
 
-const CardBasketDropdown = () => (
+const CardBasketDropdown = ({ cardItems }) => (
   <div className=" cardBasket__dropdown">
-    <div className="cardBasket__dropdown-items" />
-    <CustomButton />
+    <div className="cardBasket__dropdown-items">
+      {cardItems.map((cardItem) => (
+        <BasketItem key={cardItem.id} item={cardItem} />
+      ))}
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CardBasketDropdown;
+const mapStateToProps = ({ cardBasket: { cardItems } }) => ({
+  cardItems,
+});
+
+export default connect(mapStateToProps)(CardBasketDropdown);
