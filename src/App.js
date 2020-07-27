d51c5098ca1372e38c27bafa8472bf6ad50c498d
NatalyMak <49818793+nat081018@users.noreply.github.com>
2020-07-27 13:44:10 +0300

    add withRouter(menuItem)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
+import {Route, Switch}  from "react-router-dom"
 
 import Homepage from "./pages/homepage/homepage.component"
 
-
+const HatsPage = () => (
+  <div>HatsPage</div>
+)
 
 class App extends Component {
 
   render() {
     return(
       <div>
-        <Homepage/>
+        <Switch>
+          <Route exact path="/" component={Homepage}/>
+          <Route  path="/hats" component={HatsPage}/>
+        </Switch>
+
       </div>
     )
   }
